c20427af6525bd1b9901f15836c23ef8b5ac863c
Pierre Beucher <pierre@crafteo.io>
2022-06-26 23:10:56 +0200

    resolve variable and file content from config

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,26 @@ enum NovopsValue {
     BitwardenItem(BitwardenItem)
 }
 
+trait ResolvableNovopsValue {
+    fn resolve(&self) -> String;
+}
+
+impl ResolvableNovopsValue for NovopsValue {
+    fn resolve(&self) -> String {
+        match self {
+            NovopsValue::String(v) => return v.resolve(),
+            NovopsValue::StringValue(v) => return v.resolve(),
+            NovopsValue::BitwardenItem(v) => return v.resolve()
+        }
+    }
+}
+
+impl ResolvableNovopsValue for String {
+    fn resolve(&self) -> String {
+        return self.clone()
+    }
+}
+
 /**
  * A string set with 'value' key such as 
  * 
@@ -40,6 +60,12 @@ enum NovopsValue {
 #[derive(Debug, Deserialize)]
 struct StringValue{
     value: String
+}
+
+impl ResolvableNovopsValue for StringValue {
+    fn resolve(&self) -> String {
+        return self.value.clone();
+    }
 }
 
 /**
@@ -56,6 +82,14 @@ struct StringValue{
     bitwarden: BitwardenValue,
 }
 
+impl ResolvableNovopsValue for BitwardenItem {
+    fn resolve(&self) -> String {
+        // Dummy implementation for now
+        return format!("{}:{}", self.bitwarden.entry, self.bitwarden.field);
+    }
+}
+
+
 #[derive(Debug, Deserialize)]
  struct BitwardenValue {
     entry: String,
@@ -65,8 +99,14 @@ struct StringValue{
 
 fn main() -> Result<(), Error> {
 
-    let config = read_config(".novops.yml");
+    let config = read_config(".novops.yml").unwrap();
     println!("Found config: {:?}", config);
+
+    let (variable_map, file_content_map) = parse_config(&config, String::from("dev"));
+
+    println!("Resolved vars: {:?}", variable_map);
+    println!("Resolved files: {:?}", file_content_map);
+
     Ok(())
 }
 
@@ -76,10 +116,23 @@ fn read_config(config_path: &str) -> Result<NovopsConfig, serde_yaml::Error> {
     let config: NovopsConfig = serde_yaml::from_reader(f).unwrap();
 
     return Ok(config);
+}
 
-    // match &config.environments["dev"].variables["AWS_DEFAULT_REGION"] {
-    //     NovopsValue::String(v) => println!("Value: {}", v),
-    //     NovopsValue::StringValue(v) => println!("Value: {}", v.value),
-    //     NovopsValue::BitwardenItem(v) => println!("BitWarden: {}", v.bitwarden.entry)
-    // }
+/**
+ * Parse configuration into a concrete tuple of variable and files
+ */
+fn parse_config(config: &NovopsConfig, env_name: String) -> (HashMap<String, String>, HashMap<String, String>){
+    let env_config: &NovopsEnvironment = &config.environments[&env_name];
+
+    let mut variable_map: HashMap<String, String> = HashMap::new();
+    for (var_name, var_abstract_value) in &env_config.variables {
+        variable_map.insert(var_name.clone(), var_abstract_value.resolve());
+    }
+
+    let mut file_content_map: HashMap<String, String> = HashMap::new();
+    for file_entry in env_config.files.values() {
+        file_content_map.insert(file_entry.dest.clone(), file_entry.content.resolve());
+    }
+
+    return (variable_map, file_content_map)
 }
