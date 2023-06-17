8eee16793032381ece494272497465e28b7e3762
Pierre Beucher <pierre@crafteo.io>
2023-06-17 16:13:55 +0200

    build: dream2nix (replace cargo2nix)

--- flake.nix
+++ flake.nix
@@ -2,40 +2,49 @@
   description = "Novops flake";
 
   inputs = {
-    # cargo2nix is a more granular version of nixpkgs' buildRustPackage
-    cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
   };
 
-  outputs = { self, cargo2nix, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [cargo2nix.overlays.default];
-        };
+  outputs = { self, nixpkgs, dream2nix }:
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+      dream2nix.lib.makeFlakeOutputs {
+        systems = [ system ];
+        
+        config.projectRoot = ./.;
+        source = ./.;
+        projects = ./projects.toml;
 
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.66.1";
-          packageFun = import ./Cargo.nix;
-        };
+        packageOverrides = {
 
-      in rec {
-        devShells = {
-          default = self.packages.${system}.novops.overrideAttrs(oa: {
-            buildInputs = oa.buildInputs ++ [ 
-              cargo2nix.packages.${system}.cargo2nix
-              pkgs.pkg-config
-              pkgs.openssl.dev # required by openssl crate (transitive dep) for cargo build
-            ];
-          });
-        };
+          # Add build dependencies
+          # dream2nix uses crane and generate 2 derivations novops and novops-deps
+          # Each needs additional build inputs
+          # See https://nix-community.github.io/dream2nix/subsystems/rust.html#override-gotchas
+          novops-deps = {
+            add-deps = {
+              nativeBuildInputs = old: old ++ [
+                pkgs.pkg-config
+                pkgs.openssl.dev
+              ];
+            };
+          };
 
-        packages = {
-          novops = (rustPkgs.workspace.novops {}).bin;
-          default = packages.novops;
+          novops = {
+            add-deps = {
+              buildInputs = [
+                pkgs.openssl.dev
+                pkgs.pkg-config
+              ];
+
+              # Skip tests as most are integration tests requiring setup
+              # Tests are run outside of Nix context for now 
+              doCheck = false;
+            };
+          };
         };
-      }
-    );
+      };
 }
