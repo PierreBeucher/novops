1f8fcfbd1eda3739901065fa2fabf3939d4f4907
Pierre Beucher <pierre@crafteo.io>
2023-12-24 14:06:12 +0100

    refactor: rewrote main function for readibility

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::{process::exit, io};
+use std::io;
 
-use anyhow::Context;
+use anyhow::{Context, anyhow};
 use tokio;
-use clap::{Arg, Command, value_parser, ArgAction, crate_version};
-use novops::{self, init_logger, get_config_schema};
+use clap::{Arg, Command, value_parser, ArgAction, crate_version, ArgMatches};
+use novops::{self, init_logger, get_config_schema, NovopsLoadArgs};
 use clap_complete::{generate, Shell};
-use log::error;
 use std::collections::HashMap;
 
 // Format for novops list commands
@@ -174,145 +173,172 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let m = app.get_matches_mut();
 
-    if let Some(load_subc) = m.subcommand_matches("load") {
-
-        let symlink = load_subc.get_one::<String>("symlink").map(String::from);
-        
-        let env_format = load_subc.get_one::<String>("format")
-            .ok_or(anyhow::anyhow!("Format is None. This is probably a bug as CLI defines default value."))?.clone();
-
-        let skip_tty_check = load_subc.get_one::<bool>("skip_tty_check").map(|e| *e)
-            .ok_or(anyhow::anyhow!("skip_tty_check is None. This is probably a bug as CLI defines default value."))?.clone();
-
-        let args = novops::NovopsLoadArgs{ 
-            config: load_subc.get_one::<String>("config")
-                .ok_or(anyhow::anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone(),
-            env: load_subc.get_one::<String>("environment").map(String::from),
-            working_directory: load_subc.get_one::<String>("working_dir").map(String::from),
-            skip_working_directory_check: load_subc.get_one::<bool>("skip_workdir_check").map(|b| *b),
-            dry_run: load_subc.get_one::<bool>("dry_run").map(|e| *e)
-        };
-
-        novops::load_environment_write_vars(&args, &symlink, &env_format, skip_tty_check).await
-            .with_context(|| "Failed to load environment. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
-
-        exit(0)
-    };
-
-    if let Some(load_subc) = m.subcommand_matches("run") {
-        
-        let args = novops::NovopsLoadArgs{ 
-            config: load_subc.get_one::<String>("config")
-                .ok_or(anyhow::anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone(),
-            env: load_subc.get_one::<String>("environment").map(String::from),
-            working_directory: load_subc.get_one::<String>("working_dir").map(String::from),
-            skip_working_directory_check: load_subc.get_one::<bool>("skip_workdir_check").map(|b| *b),
-            dry_run: load_subc.get_one::<bool>("dry_run").map(|e| *e)
-        };
-
-        let command_args: Vec<&String> = load_subc.get_many::<String>("command")
-            .ok_or(anyhow::anyhow!("Command is required. This is probably a bug as CLi requires it."))?
-            .collect();
-
-        novops::load_environment_and_exec(&args, command_args).await
-            .with_context(|| "Failed to load environment and exec command. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
-
-        exit(2)
-    };
-
-    if let Some(list_subc) = m.subcommand_matches("list") {
-
-
-        if let Some(list_envs_subc) = list_subc.subcommand_matches("environments") {
-            let config_file = list_envs_subc.get_one::<String>("config")
-                .ok_or(anyhow::anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone();
-
-            let envs = novops::list_environments(&config_file).await
-                .with_context(|| "Failed to list environments. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
-
-            let output_format = list_envs_subc.get_one::<String>("format")
-                .ok_or(anyhow::anyhow!("Format is None. This is probably a bug as CLI defines default value."))?.clone();
-
-            match output_format.as_str() {
-                LIST_CMD_OUTPUT_JSON => {
-                    let json = serde_json::to_string(&envs)
-                        .with_context(|| "Failed to serialize environments to JSON. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
-                    println!("{}", json);
-                    exit(0)
-                },
-                LIST_CMD_OUTPUT_PLAIN => {
-                    println!("{}", envs.join("\n"));
-                    exit(0)
-                },
-                _ => {
-                    println!("Unknown format: {}", output_format);
-                    exit(1)
-                }
+    let result = match m.subcommand() {
+        Some(("load", load_subc)) => cmd_load(load_subc).await,
+        Some(("run", run_subc)) => cmd_run(run_subc).await,
+        Some(("list", list_subc)) => {
+            match list_subc.subcommand() {
+                Some(("environments", list_envs_subc)) => cmd_list_envs(list_envs_subc).await,
+                Some(("outputs", list_outputs_subc)) => cmd_list_outputs(list_outputs_subc).await,
+                _ => cmd_error().await,
             }
-        };
-        
-        if let Some(list_outputs_subc) = list_subc.subcommand_matches("outputs") {
-            let config_file = list_outputs_subc.get_one::<String>("config")
-                .ok_or(anyhow::anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone();
-
-            let env_name = list_outputs_subc.get_one::<String>("environment").map(String::from);
-
-            let output_format = list_outputs_subc.get_one::<String>("format")
-                .ok_or(anyhow::anyhow!("Format is None. This is probably a bug as CLI defines default value."))?.clone();
-
-            let outputs = novops::list_outputs_for_environment(&config_file, env_name).await
-                .with_context(|| "Failed to list outputs. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
-            
-            
-            // Result in the form { "variables": [ "VAR_NAME1", "VAR_NAME2" ], "files": ["/path/to/file1", "/path/to/file2", ...] }
-            let mut result: HashMap<String, Vec<String>> = HashMap::new();
-            result.insert("variables".to_string(), 
-                outputs.variables.iter().map(|e| e.1.name.clone()).collect::<Vec<String>>()
-            );
-            result.insert("files".to_string(), 
-                outputs.files.iter().map(|f| f.0.clone()).collect::<Vec<String>>()
-            );
-
-            match output_format.as_str() {
-                LIST_CMD_OUTPUT_JSON => {
-                    let json = serde_json::to_string(&result)
-                        .with_context(|| "Failed to serialize environments to JSON. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
-                    println!("{}", json);
-                    exit(0)
-                },
-                LIST_CMD_OUTPUT_PLAIN => {
-                    println!("Variables:");
-                    println!();
-                    println!("{}", result.get("variables").unwrap().join("\n"));
-                    println!();
-                    println!("Files:");
-                    println!();
-                    println!("{}", result.get("files").unwrap().join("\n"));
-                    exit(0)
-                },
-                _ => {
-                    println!("Unknown format: {}", output_format);
-                    exit(1)
-                }
-            }
-        };
+        }
+        Some(("completion", cmd)) => cmd_completion(cmd, app).await,
+        Some(("schema", schema_args)) => cmd_schema(schema_args).await,
+        _ => cmd_error().await,
     };
 
-    if let Some(cmd) = m.subcommand_matches("completion") {
-        let name = app.get_name().to_string();
-        let shell_arg = cmd.get_one::<Shell>("shell")
-            .ok_or(anyhow::anyhow!("Shell is required"))?;
-        
-        generate(*shell_arg, &mut app, name, &mut io::stdout());
-        exit(0)
-    };
-
-    if let Some(_cmd) = m.subcommand_matches("schema") {
-        let schema = get_config_schema()?;
-        println!("{}", schema);
-        exit(0)
-    }
+    result.with_context(|| "An error occured. Set environment variable RUST_LOG=[trace|debug|info|warn] or RUST_BACKTRACE=1 for more verbosity.")?;
     
-    error!("Please provide a subcommand. Use --help to see available commands.");
-    exit(1)
+    Ok(())
+}
+
+//
+// COMMAND FUNCTIONS
+// Below functions are for sub-commands of main
+//
+
+async fn cmd_list_envs(cmd_args: &ArgMatches) -> Result<(), anyhow::Error> {
+    let config_file = cmd_args.get_one::<String>("config")
+        .ok_or(anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone();
+
+    let output_format = cmd_args.get_one::<String>("format")
+        .ok_or(anyhow!("Format is None. This is probably a bug as CLI defines default value."))?.clone();
+
+    let envs = novops::list_environments(&config_file).await
+        .with_context(|| "Failed to list environments.")?;
+
+    match output_format.as_str() {
+        LIST_CMD_OUTPUT_JSON => {
+            let json = serde_json::to_string(&envs)
+                .with_context(|| "Failed to serialize environments to JSON.")?;
+            println!("{}", json);
+        },
+        LIST_CMD_OUTPUT_PLAIN => {
+            println!("{}", envs.join("\n"));
+        },
+        _ => {
+            return Err(anyhow!("Unknown format: {}", output_format));
+        }
+    }
+
+    Ok(())
+}
+
+async fn cmd_list_outputs(cmd_args: &ArgMatches) -> Result<(), anyhow::Error> {
+
+    let config_file = cmd_args.get_one::<String>("config")
+        .ok_or(anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone();
+
+    let env_name = cmd_args.get_one::<String>("environment").map(String::from);
+
+    let output_format = cmd_args.get_one::<String>("format")
+        .ok_or(anyhow!("Format is None. This is probably a bug as CLI defines default value."))?.clone();
+
+    let outputs = novops::list_outputs_for_environment(&config_file, env_name).await
+        .with_context(|| "Failed to list outputs.")?;
+    
+    
+    // Result in the form { "variables": [ "VAR_NAME1", "VAR_NAME2" ], "files": ["/path/to/file1", "/path/to/file2", ...] }
+    let mut result: HashMap<String, Vec<String>> = HashMap::new();
+    result.insert("variables".to_string(), 
+        outputs.variables.iter().map(|e| e.1.name.clone()).collect::<Vec<String>>()
+    );
+    result.insert("files".to_string(), 
+        outputs.files.iter().map(|f| f.0.clone()).collect::<Vec<String>>()
+    );
+
+    match output_format.as_str() {
+        LIST_CMD_OUTPUT_JSON => {
+            let json = serde_json::to_string(&result)
+                .with_context(|| "Failed to serialize environments to JSON.")?;
+            println!("{}", json);
+        },
+        LIST_CMD_OUTPUT_PLAIN => {
+            println!("Variables:");
+            println!();
+            println!("{}", result.get("variables").unwrap().join("\n"));
+            println!();
+            println!("Files:");
+            println!();
+            println!("{}", result.get("files").unwrap().join("\n"));
+        },
+        _ => {
+            return Err(anyhow!("Unknown output format: {}", output_format));
+        }
+    }
+
+    Ok(())
+}
+
+async fn cmd_run(cmd_args: &ArgMatches) -> Result<(), anyhow::Error> {
+    let novops_load_args = build_novops_args(cmd_args)?;
+
+    let command_args: Vec<&String> = cmd_args.get_many::<String>("command")
+        .ok_or(anyhow!("Command is required. This is probably a bug as CLi requires it."))?
+        .collect();
+
+    novops::load_environment_and_exec(&novops_load_args, command_args).await
+        .with_context(|| "Failed to load environment and exec command.")?;
+
+    Ok(())
+}   
+
+async fn cmd_load(cmd_args: &ArgMatches) -> Result<(), anyhow::Error> {
+    
+    let symlink = cmd_args.get_one::<String>("symlink").map(String::from);
+        
+    let env_format = cmd_args.get_one::<String>("format")
+        .ok_or(anyhow!("Format is None. This is probably a bug as CLI defines default value."))?.clone();
+
+    let skip_tty_check = cmd_args.get_one::<bool>("skip_tty_check").map(|e| *e)
+        .ok_or(anyhow!("skip_tty_check is None. This is probably a bug as CLI defines default value."))?.clone();
+
+    let novops_load_args = build_novops_args(cmd_args)?;
+
+    novops::load_environment_write_vars(&novops_load_args, &symlink, &env_format, skip_tty_check).await
+        .with_context(|| "Failed to load environment.")?;
+
+    Ok(())
+}
+
+async fn cmd_completion(cmd_args: &ArgMatches, mut app: Command) -> Result<(), anyhow::Error> {
+    let name = app.get_name().to_string();
+    let shell_arg = cmd_args.get_one::<Shell>("shell")
+        .ok_or(anyhow!("Shell is required"))?;
+    
+    generate(*shell_arg, &mut app, name, &mut io::stdout());
+
+    Ok(())
+}
+
+async fn cmd_schema(_: &ArgMatches) -> Result<(), anyhow::Error> {
+    let schema = get_config_schema()?;
+    println!("{}", schema);
+    Ok(())
+}
+
+async fn cmd_error() -> Result<(), anyhow::Error> {
+    Err(anyhow!("Please provide a valid subcommand. Use --help to see available commands."))
+}
+
+//
+// UTILS
+// 
+
+/**
+ * Build Novops load arguments using subcommand and expected arguments
+ * Error if subcommand does not have all expected argument for Novops load
+ */
+fn build_novops_args(cmd_args: &ArgMatches) -> Result<NovopsLoadArgs, anyhow::Error> {
+    let args = novops::NovopsLoadArgs{ 
+        config: cmd_args.get_one::<String>("config")
+            .ok_or(anyhow!("Config is None. This is probably a bug as CLI defines default value."))?.clone(),
+        env: cmd_args.get_one::<String>("environment").map(String::from),
+        working_directory: cmd_args.get_one::<String>("working_dir").map(String::from),
+        skip_working_directory_check: cmd_args.get_one::<bool>("skip_workdir_check").map(|b| *b),
+        dry_run: cmd_args.get_one::<bool>("dry_run").map(|e| *e)
+    };
+
+    Ok(args)
 }
