dd6420e468aeb9dd9846783fec45354c4b9e9d38
Pierre beucher <pierre@crafteo.io>
2024-12-07 12:46:07 +0100

    chore: added Google Workload Identity Federation test

--- tests/setup/pulumi/azure/index.ts
+++ tests/setup/pulumi/azure/index.ts
@@ -1,4 +1,6 @@
-import * as az from "@pulumi/azure-native";
+import * as az from "@pulumi/azure-native"
+import * as azuread from "@pulumi/azuread"
+import * as pulumi from "@pulumi/pulumi"
 
 const resourceGroup = new az.resources.ResourceGroup("novops-testing", {
     resourceGroupName: "novops-testing",
@@ -6,6 +8,7 @@ const resourceGroup = new az.resources.ResourceGroup("novops-testing", {
 
 const currentConfig = az.authorization.getClientConfigOutput()
 
+// Key Vault secrets from testing
 const keyVault = new az.keyvault.Vault("novops-test", {
     resourceGroupName: resourceGroup.name,
     vaultName: "novops-test",
@@ -36,3 +39,38 @@ const secret = new az.keyvault.Secret("novops-test-secret", {
         value: "v3rySecret!",
     },
 })
+
+// Entra ID app to test Google Workload Identity Federation auth
+
+const appDisplayName = "novops-test-google-workload-id-fed"
+export const identifierUri = "api://novops-test/google-workload-id-fed"
+
+const app = new azuread.Application("novops-test-google-workload-id-fed-app", {
+    displayName: appDisplayName,
+    owners: [currentConfig.objectId],
+    identifierUris: [identifierUri]
+})
+
+const servicePrincipal = new azuread.ServicePrincipal("novops-test-google-workload-id-fed-service-principal", {
+    clientId: app.clientId,
+    owners: [currentConfig.objectId]
+})
+const servicePrincipalPassword = new azuread.ServicePrincipalPassword("novops-test-google-workload-id-fed-sp-password", {
+    servicePrincipalId: servicePrincipal.id,
+    endDate: "2099-01-01T01:01:42Z"
+})
+
+const roleReader = "acdd72a7-3385-48ef-bd42-f606fba81ae7"
+const readerRoleAssignment = new az.authorization.RoleAssignment("novops-test-google-workload-id-fed-role-assignment", {
+    principalId: servicePrincipal.objectId,
+    principalType: "ServicePrincipal",
+    roleDefinitionId: pulumi.interpolate`/subscriptions/${currentConfig.subscriptionId}/providers/Microsoft.Authorization/roleDefinitions/${roleReader}`, 
+    scope: pulumi.interpolate`/subscriptions/${currentConfig.subscriptionId}`,
+})
+
+export const servicePrincipalClientId = servicePrincipal.clientId
+export const servicePrincipalObjectId = servicePrincipal.objectId
+export const tenantId = servicePrincipal.applicationTenantId
+export const applicationObjectId = app.objectId
+export const applicationClientId = app.clientId
+export const password = servicePrincipalPassword.value
--- tests/setup/pulumi/gcp/index.ts
+++ tests/setup/pulumi/gcp/index.ts
@@ -1,4 +1,8 @@
 import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
+
+const gcpConfig = new pulumi.Config("gcp");
+const projectId = gcpConfig.require("project")
 
 const secret = new gcp.secretmanager.Secret("test-secret", {
     secretId: "novops-test-secret",
@@ -11,3 +15,90 @@ const secretVersion = new gcp.secretmanager.SecretVersion("test-secret-version",
     secret: secret.id,
     secretData: "very!S3cret",
 })
+
+// Workload Identity Federation config to allow Azure authentication and test of Google WIF
+// Retrieve outputs from Azure setup stack 
+// As documented on https://cloud.google.com/iam/docs/workload-identity-federation-with-other-clouds#azure_2
+
+const azStackRef = new pulumi.StackReference("az-stack-ref", { name: "pierrebeucher/novops-test-infra-azure/test" })
+const azServicePrincipalObjectId = azStackRef.requireOutput("servicePrincipalObjectId") as pulumi.Output<string>
+const azTenantId = azStackRef.requireOutput("tenantId") as pulumi.Output<string>
+const azAppIdentifierUri = azStackRef.requireOutput("identifierUri") as pulumi.Output<string>
+
+const poolId = "novops-test-identity-pool"
+const providerId = "azure"
+
+// Use an import-if-exists and retain pattern to avoid deletion on stack down and re-import on stack up
+// Otherwise, as resources are soft-deleted, Google API would consider them already existing if Pulumi tried to recreate them
+// See https://github.com/pulumi/pulumi-gcp/issues/1149
+const identityPool = pulumi.output(gcp.iam.getWorkloadIdentityPool({ 
+    workloadIdentityPoolId: poolId,
+}).then(existingPool => 
+    new gcp.iam.WorkloadIdentityPool("workload-identity-pool", {
+        workloadIdentityPoolId: poolId,
+    }, {
+        import: existingPool.workloadIdentityPoolId,
+        retainOnDelete: true,
+    })
+).catch(e => 
+    new gcp.iam.WorkloadIdentityPool("workload-identity-pool", {
+        workloadIdentityPoolId: poolId,
+    }, { 
+        retainOnDelete: true,
+    })
+))
+
+const workloadIdentityProvider = pulumi.output(gcp.iam.getWorkloadIdentityPoolProvider({ 
+    workloadIdentityPoolProviderId: providerId, 
+    workloadIdentityPoolId: poolId
+}).then(existingWifProvider => 
+    new gcp.iam.WorkloadIdentityPoolProvider("workload-identity-pool-provider", {
+        workloadIdentityPoolProviderId: providerId,
+        workloadIdentityPoolId: poolId,
+        oidc: {
+            issuerUri: pulumi.interpolate`https://sts.windows.net/${azTenantId}/`,
+            allowedAudiences: [azAppIdentifierUri],
+        },
+        attributeMapping: {
+            "google.subject": "assertion.sub",
+        },
+    }, {
+        import: existingWifProvider.name,
+        retainOnDelete: true,
+    })
+).catch(e => 
+    new gcp.iam.WorkloadIdentityPoolProvider("workload-identity-pool-provider", {
+        workloadIdentityPoolProviderId: providerId,
+        workloadIdentityPoolId: poolId,
+        oidc: {
+            issuerUri: pulumi.interpolate`https://sts.windows.net/${azTenantId}/`,
+            allowedAudiences: [azAppIdentifierUri],
+        },
+        attributeMapping: {
+            "google.subject": "assertion.sub",
+        },
+    }, {
+        retainOnDelete: true,
+    })
+))
+
+const secretManagerIamBinding = new gcp.projects.IAMMember("secret-manager-iam-binding", {
+    project: projectId,
+    role: "roles/secretmanager.secretAccessor",
+    // Service Principal Object ID is the ID Google uses to identify or Azure user
+    member: pulumi.interpolate`principal://iam.googleapis.com/projects/${projectId}/locations/global/workloadIdentityPools/${poolId}/subject/${azServicePrincipalObjectId}`,
+}, {
+    dependsOn: [workloadIdentityProvider]
+})
+
+const viewerIamBinding = new gcp.projects.IAMMember("viewer-iam-binding", {
+    project: projectId,
+    role: "roles/viewer",
+    member: pulumi.interpolate`principal://iam.googleapis.com/projects/${projectId}/locations/global/workloadIdentityPools/${poolId}/subject/${azServicePrincipalObjectId}`,
+}, {
+    dependsOn: [workloadIdentityProvider]
+})
+
+export const workloadIdentityPoolName = identityPool.name
+export const workloadIdentityPoolProviderName = workloadIdentityProvider.name
+export const providerResourceName = workloadIdentityProvider.id
